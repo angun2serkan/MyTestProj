1a69158ca52d84d2cd3a6eefea0be75f07d9a5e0
Serkan Angun <angun.serkan2@gmail.com>
2023-07-26 16:54:35 +0300

    users route added

--- proj2/controllers/userRoutes.js
+++ proj2/controllers/userRoutes.js
@@ -5,6 +5,16 @@ const path = require("path");
 
 const dataFilePath = path.join(__dirname, "../UserDatabase.json");
 
+function readDataFromFile() {
+  const data = fs.readFileSync(dataFilePath);
+  return JSON.parse(data);
+}
+
+router.get("/users", (req, res) => {
+  const users = readDataFromFile();
+  res.send(users);
+});
+
 router.get("/test", (req, res) =>
   res.send({
     message: "Test is working",
