21f8c0408cb8ba36770db5db38112fcedc894752
Serkan Angun <angun.serkan2@gmail.com>
2023-08-03 21:34:13 +0300

    api is running

--- TodoApp/server/index.js
+++ TodoApp/server/index.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
+const PORT = 8006;
+require("dotenv").config();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "The API is working!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
