d7b93065276c9b77d60a4b58969971f6163eea34
Serkan Angun <angun.serkan2@gmail.com>
2023-08-04 15:18:40 +0300

    todoroutes implemented

--- TodoApp/server/index.js
+++ TodoApp/server/index.js
@@ -3,9 +3,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
 const PORT = 8006;
+const todoRoutes = require("./Routes/TodoRoutes");
 require("dotenv").config();
+require("./db");
 
 app.use(bodyParser.json());
+app.use("/todoroutes", todoRoutes);
 
 app.get("/", (req, res) => {
   res.json({
