90ec005e194c46521f29603b9fa198c649ec5ade
Serkan Angun <angun.serkan2@gmail.com>
2023-08-06 23:02:22 +0300

    auth middleware and create task endpoint

--- TaskManager/server/routes/taskRoutes.js
+++ TaskManager/server/routes/taskRoutes.js
@@ -1,9 +1,31 @@
 const express = require("express");
 
 const router = express.Router();
+const auth = require("../middlewares/auth");
+const Task = require("../models/Task");
 
-router.get("/", (req, res) => {
-  res.send("task routes are working");
+router.get("/test", auth, (req, res) => {
+  res.json({
+    message: "Task routes are working!",
+    user: req.user,
+  });
+});
+
+// CRUD tasks for authenticated users
+
+// create a  task
+router.post("/", auth, async (req, res) => {
+  try {
+    // description, owner
+    const task = new Task({
+      ...req.body,
+      owner: req.user._id,
+    });
+    await task.save();
+    res.status(201).json({ task, message: "Task Created Successfully" });
+  } catch (err) {
+    res.status(400).send({ error: err });
+  }
 });
 
 module.exports = router;
