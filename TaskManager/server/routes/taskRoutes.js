74a81f72cd5001ccc7f00dbdcadf2b0e5c255fb0
Serkan Angun <angun.serkan2@gmail.com>
2023-08-07 00:25:08 +0300

    update task endpoint

--- TaskManager/server/routes/taskRoutes.js
+++ TaskManager/server/routes/taskRoutes.js
@@ -61,4 +61,37 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// update a task by id
+router.patch("/:id", auth, async (req, res) => {
+  const taskid = req.params.id;
+  const updates = Object.keys(req.body);
+
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).json({ error: "Invalid Updates" });
+  }
+  try {
+    const task = await Task.findOne({
+      _id: taskid,
+      owner: req.user._id,
+    });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+
+    res.json({
+      message: "Task Updated Successfully",
+    });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+});
+
 module.exports = router;
