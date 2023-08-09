0c676b10c2ef53ebdcd8c5935ab806a9fc3d0c80
Serkan Angun <angun.serkan2@gmail.com>
2023-08-09 22:36:44 +0300

    blog model is created

---
+++ BlogApi/models/Blog.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  blog: { type: String, required: true },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
